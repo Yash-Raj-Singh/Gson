ff8b4bcf16e22c9201fabab95571d8928f0cd4fa
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-06-21 16:22:09 +0530

    Using @Expose and @Transit to hide data and provide security.

--- app/src/main/java/com/example/gson/Employee.kt
+++ app/src/main/java/com/example/gson/Employee.kt
@@ -1,31 +1,31 @@
 package com.example.gson
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 class Employee {
 
+    @Expose(serialize = false, deserialize = true)
     @SerializedName("firstName")
     private var mfirstName: String? = null
 
+    @Expose(deserialize = false)
     @SerializedName("age")
     private var mage: Int = 0
 
+    @Expose(serialize = true, deserialize = true)
     @SerializedName("mail")
     private var mmail:String? = null
 
-    @SerializedName("address")
-    private var mAddress: Address
+    @SerializedName("password")
+    @Transient private var mPassword: String? = null
 
-    @SerializedName("family")
-    private var mFamily: List<FamilyMembers>
-
-    constructor(  firstName: String,  age: Int, mail: String, address: Address, family:List<FamilyMembers> )
+    constructor(  firstName: String,  age: Int, mail: String, password: String)
     {
         mfirstName = firstName
         mage = age
         mmail = mail
-        mAddress = address
-        mFamily = family
+        mPassword = password
     }
 
 }
--- app/src/main/java/com/example/gson/MainActivity.kt
+++ app/src/main/java/com/example/gson/MainActivity.kt
@@ -3,30 +3,21 @@ package com.example.gson
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gson: Gson = Gson()
+        val gson: Gson = GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
+//
+//        val employee: Employee = Employee("John", 30, "abcc@lmail.com", "point321")
+//
+//        val json: String = gson.toJson(employee)
 
-        /*
-        val address: Address = Address("India", "New Delhi")
-
-        val family: ArrayList<FamilyMembers> = ArrayList()
-
-        family.add(FamilyMembers("Friend", 20))
-        family.add(FamilyMembers("Teacher", 0))
-        family.add(FamilyMembers("Pet", 2))
-
-        var json: String = gson.toJson(Employee("HOY", 21, "abc@gmail.com", address, family))
-        */
-
-
-        val json: String = "{\"address\":{\"city\":\"New Delhi\",\"country\":\"India\"},\"family\":[{\"age\":20,\"role\":\"Friend\"},{\"age\":0,\"role\":\"Teacher\"},{\"age\":2,\"role\":\"Pet\"}],\"age\":21,\"firstName\":\"HOY\",\"mail\":\"abc@gmail.com\"}"
+        val json: String = "{\"age\":30,\"mail\":\"abcc@lmail.com\"}"
 
         val employee: Employee = gson.fromJson(json, Employee::class.java)
-
     }
 }
